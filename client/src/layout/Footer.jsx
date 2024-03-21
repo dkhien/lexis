52221580b5427e2f65817f30bf0efcd038e22a1c
ndtd128 <95179216+ndtd128@users.noreply.github.com>
2024-03-21 17:32:18 +0700

    feat: add base layout

---
+++ client/src/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+function Footer() {
+  return (
+    <Box mt="5rem">
+      <Box sx={{ padding: '3rem', textAlign: 'center' }}>
+        &copy; 2024 Group 20 - INT2041 20
+      </Box>
+    </Box>
+  );
+}
+
+export default Footer;
