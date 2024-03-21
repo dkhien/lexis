196feb0d23e934fe9c9df5ca8f09be401c5f85cd
ndtd128 <95179216+ndtd128@users.noreply.github.com>
2024-03-21 17:31:54 +0700

    feat: add theme

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
