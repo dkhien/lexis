c4dad62945cc3f027be7e3f68f2a011375b82fc8
Hien Doan <dkhien.work@gmail.com>
2024-03-24 20:46:08 +0700

    feat: front-end routing base
    Squashed commit of the following:
    commit 2091d84662bfe7668bf355545dcbdfb6bd84ba3e
    Author: Hien Doan <dkhien.work@gmail.com>
    Date:   Sun Mar 24 20:42:06 2024 +0700
    
        chore: remove eslint-disable comment and redundant route property
    
    commit a1b733082870411cfe9106b8c5ecdb726123d767
    Author: macabo10 <macabo10quymui@gmail.com>
    Date:   Sun Mar 24 20:30:38 2024 +0700
    
        [Fix]: Minor change
    
    commit 507552a0d3ec85c4897d8b424758ef785457a47b
    Author: macabo10 <macabo10quymui@gmail.com>
    Date:   Fri Mar 22 19:31:17 2024 +0700
    
        [Feature]: Add NotFound page and change reading to reader
    
    commit 4ea69692421307771ccbe5ff7c97d6d033003513
    Author: macabo10 <macabo10quymui@gmail.com>
    Date:   Fri Mar 22 18:59:31 2024 +0700
    
        [Feature]: Add routing to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
 import './App.css';
-import { Typography } from '@mui/material';
+import Home from './pages/Home';
+import Upload from './pages/Upload';
+import Reader from './pages/Reader';
+import NotFound from './pages/NotFound';
 import MainLayout from './layout/MainLayout';
 
 function App() {
+  const routes = [
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/upload',
+      element: <Upload />,
+    },
+    {
+      path: '/reader',
+      element: <Reader />,
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ];
   return (
-    <MainLayout>
-      <Typography>Home</Typography>
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.element}
+            />
+          ))}
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Upload.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Upload() {
+  return (
+    <h1>Upload</h1>
+  );
+}
+
+export default Upload;
