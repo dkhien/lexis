95b94349fca020e346744affc71dda666eacf377
tranngocbach <tranngocbach2003@gmail.com>
2024-04-05 11:56:35 +0700

    feat:test css for dyslexia

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './Home.css'
 function Home() {
   return (
-    <h1>Home</h1>
-  );
+      <div className="container">
+        <h1>Welcome to Lexis</h1>
+        <h2>Subheading</h2>
+        <p>This is a paragraph with some text. This is to demonstrate the CSS styling for paragraph tags.</p>
+        <ul>
+          <li>List item 1</li>
+          <li>List item 2</li>
+        </ul>
+        <a href="#!">This is a hyperlink</a>
+      </div>
+)
+  ;
 }
 
 export default Home;
