d2b7dd422bc5ec3ddb76e91d3c251c719a52facd
macabo10 <macabo10quymui@gmail.com>
2024-04-08 13:55:28 +0700

    [Fix]: Minor change

--- server/services/ocr.js
+++ server/services/ocr.js
@@ -1,7 +1,21 @@
 const tesseract = require('node-tesseract-ocr');
 
 function imageToText(image, language) {
-  return tesseract.recognize(image, { lang: language });
+  return tesseract.recognize(
+    image,
+    {
+      lang: language,
+      // Automatic page segmentation with OSD
+      oem: 1,
+      // Assume a single uniform block of text.
+      psm: 3,
+    },
+  );
+}
+
+function imagesToText(images, language) {
+  return Promise.all(images.map((image) => imageToText(image, language)));
 }
 
 module.exports = imageToText;
+module.exports = imagesToText;
