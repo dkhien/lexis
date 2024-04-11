03f8846b9ec8dc05cbdf763cd718bcd25561159e
Hien Doan <dkhien.work@gmail.com>
2024-04-11 22:56:01 +0700

    feat: normalize uploaded file name

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -4,6 +4,25 @@ const fs = require('fs');
 const uuidv4 = require('uuid').v4;
 const { Directory } = require('../utils/fileUtils');
 
+/**
+ * Normalizes a file name to avoid file operation errors.
+ *
+ * @param {string} fileName - The original file name.
+ * @returns {string} The normalized file name.
+ */
+const normalizeFileName = (fileName) => {
+  // Remove accents (for example, in Vietnamese words)
+  let normalizedFileName = fileName.normalize('NFKD').replace(/[\u0300-\u036f]/g, '');
+
+  // Replace spaces with hyphens
+  normalizedFileName = normalizedFileName.replace(/\s/g, '-');
+
+  // Remove all special characters
+  normalizedFileName = normalizedFileName.replace(/[^a-zA-Z0-9-]/g, '');
+
+  return normalizedFileName;
+};
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(__dirname, '../', Directory.UPLOADS);
@@ -13,7 +32,9 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const randomId = uuidv4();
-    const fileName = `${randomId}-${Date.now()}-${file.originalname.replace(/\s/g, '-')}`;
+    const originalFileName = normalizeFileName(file.originalname);
+    const fileName = `${randomId}-${Date.now()}-${originalFileName}`;
+
     cb(null, fileName);
   },
 });
