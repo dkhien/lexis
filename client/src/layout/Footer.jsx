3fe1b937cd0b43b701708c39f3d4226ab1e259f4
Hien Doan <dkhien.work@gmail.com>
2024-05-10 21:24:41 +0700

    fix: revert to old footer styling to avoid footer covering main content

--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -3,16 +3,7 @@ import { Box } from '@mui/material';
 
 function Footer() {
   return (
-    <Box
-      sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        width: '100%',
-        padding: '3rem',
-        textAlign: 'center',
-      }}
-    >
+    <Box sx={{ padding: '3rem', textAlign: 'center' }}>
       &copy; 2024 Group 20 - INT2041 20
     </Box>
   );
